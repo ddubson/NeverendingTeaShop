85ce872ab50538f48126e0caac0faf85fb22e1eb
Dmitriy Dubson <ddubson@pivotal.io>
2019-10-14 13:27:37 -0400

    Add Fetch all product items query test case

---
+++ run-good-product-api.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Go to base directory
+pushd $(git rev-parse --show-toplevel)
+
+# Run the API project
+dotnet run --project src/GoodProduct.API
--- src/GoodProduct.Application/ProductItems/Queries/FetchAllProductItemsQuery.cs
+++ src/GoodProduct.Application/ProductItems/Queries/FetchAllProductItemsQuery.cs
@@ -8,9 +8,6 @@ namespace GoodProduct.Application.ProductItems.Queries
     public class FetchAllProductItemsQuery : IFetchAllProductItemsQuery
     {
         public Task<T> Execute<T>(IFetchAllProductItemsOutcomeHandler<Task<T>> outcomeHandler)
-        {
-            return outcomeHandler.ReceivedAllProducts(new List<ProductItem>
-                {new ProductItem("Skillet 20oz")});
-        }
+            => outcomeHandler.ReceivedAllProducts(new List<ProductItem> {new ProductItem("Skillet 20oz")});
     }
 }
